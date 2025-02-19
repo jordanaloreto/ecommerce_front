0df1a7d2038aadbc4676db1dbeb8fd0f308dde59
Jordana Loreto <jordanaloreto45@gmail.com>
2025-02-19 20:50:57 -0300

    review

--- lib/screens/product_details_screen.dart
+++ lib/screens/product_details_screen.dart
@@ -1,26 +1,78 @@
-import 'package:ecommerce_front/controllers/cart_controller.dart';
 import 'package:ecommerce_front/screens/ask_question_screen.dart';
-import 'package:ecommerce_front/utils/app_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:ecommerce_front/controllers/cart_controller.dart';
+import 'package:ecommerce_front/utils/app_storage.dart';
 import '../models/product.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class ProductDetailsScreen extends StatelessWidget {
+class ProductDetailsScreen extends StatefulWidget {
   final Product product;
 
   const ProductDetailsScreen({required this.product});
 
   @override
+  _ProductDetailsScreenState createState() => _ProductDetailsScreenState();
+}
+
+class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
+  double _rating = 3.0; // Valor inicial da nota
+  bool _isSubmitting = false; // Estado do envio da avaliação
+Future<Map<String, String>> _getHeaders() async {
+    final token = AppStorage.instance.token;
+    if (token != null) {
+      return {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer $token",
+      };
+    }
+    return {"Content-Type": "application/json"};
+  }
+  Future<void> _submitReview() async {
+    setState(() {
+      _isSubmitting = true;
+    });
+
+    final userId = AppStorage().getUserId(); // Pegue o ID do usuário logado
+    final url = Uri.parse('http://localhost:8000/review/save'); // Altere para a URL do seu backend
+
+    final response = await http.post(
+      url,
+      headers: await _getHeaders(),
+      body: jsonEncode({
+        "product_id": widget.product.id,
+        "user_id": userId,
+        "rating": _rating,
+      }),
+    );
+
+    setState(() {
+      _isSubmitting = false;
+    });
+
+    if (response.statusCode == 200) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Avaliação enviada com sucesso!")),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Erro ao enviar a avaliação.")),
+      );
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(product.name),
+        title: Text(widget.product.name),
       ),
       body: Center(
         child: LayoutBuilder(
           builder: (context, constraints) {
-            double cardWidth =
-                constraints.maxWidth > 600 ? 600 : constraints.maxWidth;
+            double cardWidth = constraints.maxWidth > 600 ? 600 : constraints.maxWidth;
             return SingleChildScrollView(
               child: Container(
                 width: cardWidth,
@@ -33,7 +85,6 @@ class ProductDetailsScreen extends StatelessWidget {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      // Imagem do produto
                       ClipRRect(
                         borderRadius: const BorderRadius.vertical(
                           top: Radius.circular(16),
@@ -41,76 +92,69 @@ class ProductDetailsScreen extends StatelessWidget {
                         child: Image.network(
                           "assets/images/10771017.png",
                           width: double.infinity,
-                          height: 400, // Altura aumentada para mais retangular
+                          height: 400,
                           fit: BoxFit.cover,
                         ),
                       ),
-                      // Informações do produto
                       Padding(
                         padding: const EdgeInsets.all(16.0),
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Text(
-                              product.name,
-                              style: TextStyle(
-                                fontSize: 24,
-                                fontWeight: FontWeight.bold,
-                              ),
+                              widget.product.name,
+                              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                             ),
                             SizedBox(height: 8),
                             Text(
-                              "R\$${product.price.toStringAsFixed(2)}",
-                              style: TextStyle(
-                                fontSize: 20,
-                                color: Colors.green,
-                                fontWeight: FontWeight.bold,
-                              ),
+                              "R\$${widget.product.price.toStringAsFixed(2)}",
+                              style: TextStyle(fontSize: 20, color: Colors.green, fontWeight: FontWeight.bold),
+                            ),
+                            
+                            
+                            SizedBox(height: 16),
+                            // Componente de Avaliação
+                            Text("Avalie este produto:", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                            RatingBar.builder(
+                              initialRating: _rating,
+                              minRating: 1,
+                              direction: Axis.horizontal,
+                              allowHalfRating: true,
+                              itemCount: 5,
+                              itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                              itemBuilder: (context, _) => Icon(Icons.star, color: Colors.amber),
+                              onRatingUpdate: (rating) {
+                                setState(() {
+                                  _rating = rating;
+                                });
+                              },
                             ),
                             SizedBox(height: 16),
-                            Text(
-                              "Subcategoria: ${product.subCategoria.name}",
-                              style: TextStyle(fontSize: 16),
-                            ),
-                            SizedBox(height: 8),
-                            Text(
-                              "Formas de pagamento: Cartão, Boleto, Pix",
-                              style: TextStyle(fontSize: 16),
-                            ),
-                            SizedBox(height: 8),
-                            Row(
-                              children: [
-                                Icon(Icons.star, color: Colors.amber),
-                                SizedBox(width: 4),
-                                Text(
-                                  "4.5",
-                                  style: TextStyle(fontSize: 16),
+                            Center(
+                              child: ElevatedButton(
+                                onPressed: _isSubmitting ? null : _submitReview,
+                                child: _isSubmitting
+                                    ? CircularProgressIndicator(color: Colors.white)
+                                    : Text("Enviar Avaliação"),
+                                style: ElevatedButton.styleFrom(
+                                  padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
                                 ),
-                              ],
+                              ),
                             ),
                             SizedBox(height: 16),
                             Center(
                               child: ElevatedButton.icon(
                                 onPressed: () {
-                                  Provider.of<CartController>(context,
-                                          listen: false)
-                                      .addProductToCart(
-                                          AppStorage().getUserId(),
-                                          product.id,
-                                          1);
+                                  Provider.of<CartController>(context, listen: false)
+                                      .addProductToCart(AppStorage().getUserId(), widget.product.id, 1);
                                   ScaffoldMessenger.of(context).showSnackBar(
-                                    SnackBar(
-                                        content: Text(
-                                            "Produto adicionado ao carrinho!")),
+                                    SnackBar(content: Text("Produto adicionado ao carrinho!")),
                                   );
                                 },
                                 icon: Icon(Icons.shopping_cart),
                                 label: Text("Adicionar ao Carrinho"),
                                 style: ElevatedButton.styleFrom(
-                                  padding: const EdgeInsets.symmetric(
-                                    horizontal: 24,
-                                    vertical: 12,
-                                  ),
+                                  padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
                                 ),
                               ),
                             ),
@@ -119,7 +163,7 @@ class ProductDetailsScreen extends StatelessWidget {
                                 Navigator.push(
                                   context,
                                   MaterialPageRoute(
-                                    builder: (context) => AskQuestionScreen(productId: product.id),
+                                    builder: (context) => AskQuestionScreen(productId: widget.product.id),
                                   ),
                                 );
                               },
